2bcfed83517cfc772686a8878ac6890e1540c514
Takurus <developer@takurus.com>
2022-06-30 03:53:13 -0400

    Initial Commit

---
+++ Word Blast/Word_BlastApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_BlastApp.swift
+//  Word Blast
+//
+//  Created by Jack Farray on 2022-06-29.
+//
+
+import SwiftUI
+
+@main
+struct Word_BlastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
